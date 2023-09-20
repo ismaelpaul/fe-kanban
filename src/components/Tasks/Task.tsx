3d9d9bee4ebf15d6ef33d54815ce693421f54bfc
ismaelpaul <ismaafp@gmail.com>
2023-09-20 20:00:50 +0100

    Edit to trigger the render of the TaskModal component

--- src/components/Tasks/Task.tsx
+++ src/components/Tasks/Task.tsx
@@ -1,24 +1,37 @@
+import { useState } from 'react';
 import { SingleTask } from '../../interfaces/ITask';
 import Card from '../Card/Card';
+import TaskModal from './TaskModal';
 
 interface TaskPros {
 	task: SingleTask;
 }
 const Task = ({ task }: TaskPros) => {
+	const [isTaskModalOpen, setIsTaskModalOpen] = useState(false);
+
 	const cardClass =
 		'bg-white drop-shadow-card w-[17.5rem] px-4 py-6 rounded-lg';
 
+	const handleTaskClick = () => {
+		setIsTaskModalOpen(true);
+	};
+
 	return (
-		<>
+		<div onClick={handleTaskClick}>
 			<Card cardClass={cardClass}>
-				<div>
+				<>
 					<h2 className="text-m-heading">{task.title}</h2>
 					<span className="text-body font-bold text-medium-grey">
 						0 of 3 subtasks
 					</span>
-				</div>
+				</>
 			</Card>
-		</>
+			{isTaskModalOpen ? (
+				<TaskModal task={task} setIsTaskModalOpen={setIsTaskModalOpen} />
+			) : (
+				<></>
+			)}
+		</div>
 	);
 };
 
