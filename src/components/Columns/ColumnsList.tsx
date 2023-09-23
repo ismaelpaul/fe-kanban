25812c7f3d0f3e84beb1509df05870be950e163f
ismaelpaul <ismaafp@gmail.com>
2023-09-23 20:09:04 +0100

    Add New Column column

--- src/components/Columns/ColumnsList.tsx
+++ src/components/Columns/ColumnsList.tsx
@@ -5,10 +5,10 @@ import { IColumns } from '../../interfaces/IColumn';
 import useBoardStore from '../../store/boardStore';
 import { useQueryClient } from 'react-query';
 import Column from './Column';
+import EmptyBoard from '../Boards/EmptyBoard';
 
 const ColumnsList = () => {
 	const boardId = useBoardStore((state) => state.boardId);
-	console.log(boardId, '<<< board id');
 
 	const queryClient = useQueryClient();
 
@@ -18,7 +18,6 @@ const ColumnsList = () => {
 		queryKey: queryKey,
 		queryFn: () => getColumnsByBoardId(boardId),
 	});
-	console.log(data);
 
 	useEffect(() => {
 		queryClient.invalidateQueries('columns');
@@ -34,17 +33,24 @@ const ColumnsList = () => {
 
 	const { columns }: IColumns = data || { columns: [] };
 
-	return (
-		<div className="flex gap-6 overflow-x-scroll h-screen bg-light-bg px-4 py-8">
-			{columns.map((column, index) => {
-				console.log(column.column_id);
+	if (columns.length === 0) {
+		return <EmptyBoard />;
+	}
 
+	return (
+		<div className="flex gap-6 overflow-x-scroll h-screen bg-light-bg dark:bg-dark-bg px-4 py-8 mt-16">
+			{columns.map((column, index) => {
 				return (
 					<React.Fragment key={column.column_id}>
 						<Column index={index} column={column} />
 					</React.Fragment>
 				);
 			})}
+			<div className="bg-gradient-to-b from-linear to-linear-50 dark:from-dark-grey dark:to-dark-grey-50 w-[17.5rem] flex items-center rounded-md mt-12">
+				<span className="text-l-heading block w-[17.5rem] text-medium-grey text-center">
+					+ New Column
+				</span>
+			</div>
 		</div>
 	);
 };
