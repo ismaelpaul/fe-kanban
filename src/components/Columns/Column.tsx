1dfbc97734b38d829dd0070c57c4c3b03f64f763
ismaelpaul <ismaafp@gmail.com>
2023-09-24 19:51:05 +0100

    Change variable name

--- src/components/Columns/Column.tsx
+++ src/components/Columns/Column.tsx
@@ -8,26 +8,26 @@ interface ColumnProps {
 }
 const Column = ({ column, index }: ColumnProps) => {
 	const [tasksLength, setTasksLength] = useState(0);
-	let bgColor;
+	let circleColor;
 
 	if (index === 0) {
-		bgColor = 'bg-blue-dot';
+		circleColor = 'bg-blue-dot';
 	} else if (index === 1) {
-		bgColor = 'bg-purple-dot';
+		circleColor = 'bg-purple-dot';
 	} else {
-		bgColor = 'bg-green-dot';
+		circleColor = 'bg-green-dot';
 	}
 	return (
 		<div>
 			<div className="flex gap-3 items-center">
 				<div
-					className={` ${bgColor} w-[0.938rem] h-[0.938rem] rounded-full`}
+					className={` ${circleColor} w-[0.938rem] h-[0.938rem] rounded-full`}
 				></div>
 				<h1 className="text-s-heading text-medium-grey tracking-2.4px">
 					{column.name.toUpperCase()} ({tasksLength})
 				</h1>
 			</div>
-			<div className="flex flex-col gap-5 py-8">
+			<div className="flex flex-col gap-5 py-8 w-[17.5rem]">
 				<TaskList columnId={column.column_id} setTasksLength={setTasksLength} />
 			</div>
 		</div>
