15f82212e1536a6448d68e35c5b7a9ac1baf243a
ismaelpaul <ismaafp@gmail.com>
2023-09-26 21:36:59 +0100

    Add handleDeleteModal function

--- src/components/KebabMenu/KebabMenuModal.tsx
+++ src/components/KebabMenu/KebabMenuModal.tsx
@@ -2,22 +2,43 @@ interface KebabMenuProps {
 	editText: string;
 	deleteText: string;
 	menuPosition: string;
+	setIsKebabMenuOpen: (arg: boolean) => void;
+	setIsDeleteModalOpen: (arg: boolean) => void;
+	setIsTaskModalOpen?: (arg: boolean) => void;
 }
 
 const KebabMenuModal = ({
 	editText,
 	deleteText,
 	menuPosition,
+	setIsKebabMenuOpen,
+	setIsDeleteModalOpen,
+	setIsTaskModalOpen,
 }: KebabMenuProps) => {
+	const handleDeleteModal = () => {
+		setIsKebabMenuOpen(false);
+		console.log('task modal');
+
+		setIsTaskModalOpen?.(false);
+		setIsDeleteModalOpen(true);
+	};
+
 	return (
-		<div
-			className={`flex flex-col gap-4 pl-4 py-[1.375rem] w-48 bg-white rounded-lg absolute drop-shadow-card ${menuPosition}`}
-		>
-			<span className="text-l-body text-medium-grey cursor-pointer">
-				{editText}
-			</span>
-			<span className="text-l-body text-red cursor-pointer">{deleteText}</span>
-		</div>
+		<>
+			<div
+				className={`flex flex-col gap-4 pl-4 py-[1.375rem] w-48 bg-white rounded-lg absolute drop-shadow-card ${menuPosition}`}
+			>
+				<span className="text-l-body text-medium-grey cursor-pointer">
+					{editText}
+				</span>
+				<span
+					onClick={handleDeleteModal}
+					className="text-l-body text-red cursor-pointer"
+				>
+					{deleteText}
+				</span>
+			</div>
+		</>
 	);
 };
 
