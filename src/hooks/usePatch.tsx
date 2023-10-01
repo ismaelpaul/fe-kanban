f7916ddc31920d4544cb6a6e01e92e1e0dd544b1
ismaelpaul <ismaafp@gmail.com>
2023-10-01 17:21:38 +0100

    Update react-query

--- src/hooks/usePatch.tsx
+++ src/hooks/usePatch.tsx
@@ -1,4 +1,4 @@
-import { useMutation } from 'react-query';
+import { useMutation } from '@tanstack/react-query';
 
 interface PatchProps {
 	patchFn: (resourceId: number, updatedData: object) => Promise<void>;
