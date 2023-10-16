15be11f1141f09b04bf7127fecd069200bb9d5f3
ismaelpaul <ismaafp@gmail.com>
2023-10-16 20:15:07 +0100

    Finish Dropdown component

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import { useState } from 'react';
 import ChevronUp from '../SVGComponents/ChevronUp';
 import ChevronDown from '../SVGComponents/ChevronDown';
-
-interface Options {
-	label: string;
-	value: string;
-}
+import { Options } from '../../interfaces/IOptionsDropdown';
 
 interface DropDownProps {
-	selectedOption: string;
+	selectedOption: Options;
 	options: Options[];
 	isDisabled: boolean;
-	setSelectedOption: (arg: string) => void;
+	setSelectedOption: (arg: Options) => void;
 }
 const Dropdown = ({
 	selectedOption,
@@ -23,17 +19,20 @@ const Dropdown = ({
 	const [isOpen, setIsOpen] = useState(false);
 
 	const handleOptionClick = (option: Options) => {
-		setSelectedOption(option.label);
+		setSelectedOption(option);
 		setIsOpen(false);
 	};
 
 	return (
-		<div className="flex items-center gap-2 relative cursor-pointer">
+		<div className="flex items-center gap-2 relative cursor-pointer mt-2">
 			<button
+				type="button"
 				className="flex items-center justify-between border border-medium-grey border-opacity-25 px-4 py-3 rounded w-full"
 				onClick={() => (!isDisabled ? setIsOpen(!isOpen) : '')}
 			>
-				<span className="text-l-body dark:text-white">{selectedOption}</span>
+				<span className="text-l-body dark:text-white leading-[100%]">
+					{selectedOption.label}
+				</span>
 				{isOpen ? <ChevronUp /> : <ChevronDown />}
 			</button>
 			{isOpen && (
