c11bb291412aec270354123a7ccc2af7c5c50408
ismaelpaul <ismaafp@gmail.com>
2023-10-16 20:16:44 +0100

    Add TaskSubmit interface

--- src/interfaces/ITask.tsx
+++ src/interfaces/ITask.tsx
@@ -1,3 +1,5 @@
+import { SubtaskSubmit } from './ISubtask';
+
 export interface SingleTask {
 	column_id: number;
 	task_id: number;
@@ -8,4 +10,12 @@ export interface SingleTask {
 
 export interface ITasks {
 	tasks: SingleTask[];
+}
+
+export interface TaskSubmit {
+	column_id: number;
+	title: string;
+	description: string;
+	status: string;
+	subtasks: SubtaskSubmit[];
 }
