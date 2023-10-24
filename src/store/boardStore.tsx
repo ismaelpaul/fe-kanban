d920d3214db4d7b34e2abf15bc4e0972b01f2a28
ismaelpaul <ismaafp@gmail.com>
2023-10-24 21:44:32 +0100

    Add selectedBoard and setSelectedBoard

--- src/store/boardStore.tsx
+++ src/store/boardStore.tsx
@@ -2,8 +2,11 @@ import { create } from 'zustand';
 import { BoardStoreActions, BoardStoreState } from '../interfaces/IBoardStore';
 
 const useBoardStore = create<BoardStoreState & BoardStoreActions>((set) => ({
-	boardId: 0,
+	boardId: 1,
 	setBoardId: (newBoardId: number) => set({ boardId: newBoardId }),
+	selectedBoard: '',
+	setSelectedBoard: (newSelectedBoard: string) =>
+		set({ selectedBoard: newSelectedBoard }),
 }));
 
 export default useBoardStore;
