a964163fd94f50a66ef89b0dfddf81c1f26cd935
ismaelpaul <ismaafp@gmail.com>
2023-10-26 21:07:26 +0100

    Add useWindowDimensions custom hook to check if isMobile and add classes conditionally

--- src/components/Columns/ColumnsList.tsx
+++ src/components/Columns/ColumnsList.tsx
@@ -6,8 +6,12 @@ import { useQueryClient } from '@tanstack/react-query';
 import Column from './Column';
 import EmptyBoard from '../Boards/EmptyBoard';
 import useBoardStore from '../../store/boardStore';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
 
-const ColumnsList = () => {
+interface ColumnsListProps {
+	isAllBoardsOpen: boolean;
+}
+const ColumnsList = ({ isAllBoardsOpen }: ColumnsListProps) => {
 	const boardId = useBoardStore((state) => state.boardId);
 
 	const queryClient = useQueryClient();
@@ -18,6 +22,9 @@ const ColumnsList = () => {
 		queryKey: queryKey,
 		queryFn: () => getColumnsByBoardId(boardId),
 	});
+
+	const { width } = useWindowDimensions();
+	const isMobile = width < 768;
 
 	useEffect(() => {
 		queryClient.invalidateQueries(['columns', boardId]);
@@ -38,7 +45,11 @@ const ColumnsList = () => {
 	}
 
 	return (
-		<div className="flex gap-6 overflow-x-scroll h-screen bg-light-bg dark:bg-dark-bg px-4 py-8 mt-16">
+		<div
+			className={`flex gap-6 overflow-x-scroll h-screen bg-light-bg dark:bg-dark-bg ${
+				isAllBoardsOpen && !isMobile ? 'ml-[16.5rem]' : 'ml-0'
+			} px-4 py-8 mt-16`}
+		>
 			{columns.map((column, index) => {
 				return (
 					<React.Fragment key={column.column_id}>
