344182e8b508fbe7cba44fe7c029ab79a91d673b
ismaelpaul <ismaafp@gmail.com>
2023-11-02 21:15:20 +0000

    Add selectedBoard and setSelectedBoard properties

--- src/interfaces/IBoardStore.tsx
+++ src/interfaces/IBoardStore.tsx
@@ -1,7 +1,9 @@
 export interface BoardStoreState {
 	boardId: number;
+	selectedBoard: string;
 }
 
 export interface BoardStoreActions {
 	setBoardId: (newBoardId: number) => void;
+	setSelectedBoard: (newSelectedBoard: string) => void;
 }
