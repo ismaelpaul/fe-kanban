78b92d986beb285585880f0f12b8f7b2de4b5a6f
ismaelpaul <ismaafp@gmail.com>
2023-12-14 21:11:06 +0000

    Add onClick event
    Will be passed down as props

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,17 @@
 interface CardProps {
 	children: JSX.Element;
 	cardClass: string;
+	onClick: () => void;
 }
 
-const Card = ({ children, cardClass }: CardProps) => {
-	return <article className={`${cardClass}`}>{children}</article>;
+const Card = ({ children, cardClass, onClick }: CardProps) => {
+	console.log(onClick);
+
+	return (
+		<article onClick={onClick} className={`${cardClass}`}>
+			{children}
+		</article>
+	);
 };
 
 export default Card;
