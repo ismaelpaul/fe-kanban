ad44c4361f4055145270347aab08044d94ffe98c
ismaelpaul <ismaafp@gmail.com>
2023-12-27 10:09:42 +0000

    Add TextInput and TextAreaInput reusable input components

---
+++ src/components/Input/TextAreaInput.tsx
@@ -0,0 +1,19 @@
+import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
+
+type TextInputProps<T extends FieldValues> = {
+	register: UseFormRegister<T>;
+	name: Path<T>;
+	className: string;
+	placeholder?: string | undefined;
+	defaultValue: string | undefined;
+};
+
+const TextAreaInput = <T extends FieldValues>({
+	register,
+	name,
+	...inputProps
+}: TextInputProps<T>) => {
+	return <textarea {...register(name)} {...inputProps} />;
+};
+
+export default TextAreaInput;
