ac0b1e9bc78867b450e62dd4b0f384dbb4e00ff0
ismaelpaul <ismaafp@gmail.com>
2023-12-28 17:13:06 +0000

    Add is_new property and set subtask_id and placeholder properties as optional
    These properties will be removed before sending to the server

--- src/models/Subtask.tsx
+++ src/models/Subtask.tsx
@@ -1,8 +1,9 @@
 import { z } from 'zod';
 
 export const SubtaskSchema = z.object({
-	subtask_id: z.number(),
+	subtask_id: z.number().optional(),
 	title: z.string().nonempty({ message: "Can't be empty" }),
 	is_completed: z.boolean().default(false),
-	placeholder: z.string(),
+	placeholder: z.string().optional(),
+	is_new: z.boolean().optional(),
 });
