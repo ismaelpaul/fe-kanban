e09db0c2a3631614da08cb75ada7db0edc775f55
ismaelpaul <ismaafp@gmail.com>
2024-01-09 20:33:28 +0000

    Add getEditSubtask and getNewSubtasksToAdd function

--- src/utils/Task/TaskSubmission.tsx
+++ src/utils/Task/TaskSubmission.tsx
@@ -3,11 +3,12 @@ import {
 	addNewSubtaskByTaskId,
 	addNewTask,
 	deleteSubtask,
+	updateSubtaskTitleByid,
 	updateTaskPositionAndStatus,
 	updateTaskTitleAndDescription,
 } from '../../api/kanbanApi';
 import { TaskStatusDropdown, TaskSubmit } from '../../interfaces/ITask';
-import { SubtaskInput } from '../../interfaces/ISubtask';
+import { SingleSubtask, SubtaskInput } from '../../interfaces/ISubtask';
 
 type UpdatedTaskTitleAndDescription = {
 	title?: string;
@@ -39,60 +40,75 @@ export const editTaskSubmission = async (
 	newTaskData: Partial<TaskSubmit>,
 	selectedOption: TaskStatusDropdown,
 	taskData: Partial<TaskSubmit>,
-	queryClient: QueryClient
+	queryClient: QueryClient,
+	subtasksToDelete: number[],
+	setSubtasksToDelete: (arg: number[]) => void
 ) => {
-	const taskId: number = taskData.task_id ?? 0;
+	const taskId: number = taskData?.task_id ?? 0;
 
 	newTaskData.status = selectedOption.value;
 
 	// Edit title and description
 	const updatedTaskTitleAndDescription: UpdatedTaskTitleAndDescription = {};
-	if (newTaskData.title !== taskData.title) {
+	if (newTaskData.title !== taskData?.title) {
 		updatedTaskTitleAndDescription.title = newTaskData.title;
 	}
-	if (newTaskData.description !== taskData.description) {
+	if (newTaskData.description !== taskData?.description) {
 		updatedTaskTitleAndDescription.description = newTaskData.description;
 	}
 
 	await updateTaskTitleAndDescription(taskId, updatedTaskTitleAndDescription);
 
-	// Edit subtasks
-	if (newTaskData.subtasks) {
-		const newSubtasks = newTaskData.subtasks.filter(
-			(subtask) => subtask.is_new
-		);
-		if (newSubtasks.length > 0) {
-			newSubtasks.forEach((subtask) => {
+	// Edit position and status
+	if (newTaskData.status !== taskData?.status) {
+		const columnId = newTaskData.column_id;
+		const positionAndStatusData = {
+			currentColumnId: columnId,
+			newColumnId: selectedOption.id,
+			currentTaskPosition: taskData?.position,
+			newTaskPosition: null,
+			newStatus: newTaskData.status,
+		};
+		await updateTaskPositionAndStatus(taskId, positionAndStatusData);
+	}
+
+	const subtasks = taskData.subtasks;
+	const newSubtasks = newTaskData.subtasks;
+
+	const subtasksHaveChanged = compareSubtasks(subtasks, newSubtasks);
+
+	if (subtasksHaveChanged) {
+		if (subtasksToDelete.length > 0) {
+			await deleteSubtask(subtasksToDelete);
+			setSubtasksToDelete([]);
+		}
+
+		// Edit subtasks
+		const editedSubtasks = getEditedSubtask(subtasks, newSubtasks);
+
+		if (editedSubtasks.length > 0) {
+			editedSubtasks.forEach((subtask) => {
+				const subtaskId = subtask.subtask_id;
+				updateSubtaskTitleByid(subtaskId, subtask);
+			});
+		}
+
+		// Add new subtasks
+		const newSubtasksToAdd = getNewSubtasksToAdd(newSubtasks);
+
+		if (newSubtasksToAdd.length > 0) {
+			newSubtasksToAdd.forEach((subtask: SubtaskInput) => {
 				delete subtask.placeholder;
 				delete subtask.subtask_id;
 				delete subtask.is_new;
 			});
-			await addNewSubtaskByTaskId(taskId, newSubtasks);
-		}
-
-		const subtasksToBeDeleted = getSubtasksToBeDeleted(taskData, newTaskData);
-
-		if (subtasksToBeDeleted.length > 0) {
-			await deleteSubtask(subtasksToBeDeleted);
-		}
-
-		// Edit position and status
-		if (newTaskData.status !== taskData.status) {
-			const columnId = newTaskData.column_id;
-			const positionAndStatusData = {
-				currentColumnId: columnId,
-				newColumnId: selectedOption.id,
-				currentTaskPosition: taskData.position,
-				newTaskPosition: null,
-				newStatus: newTaskData.status,
-			};
-			await updateTaskPositionAndStatus(taskId, positionAndStatusData);
+			await addNewSubtaskByTaskId(taskId, newSubtasksToAdd);
 		}
 	}
 
 	const queryKey = 'tasks';
 
-	const columnId = taskData.column_id;
+	const columnId = taskData?.column_id;
 	const newColumnId = selectedOption.id;
 
 	if (newColumnId !== columnId) {
@@ -102,26 +118,61 @@ export const editTaskSubmission = async (
 	queryClient.invalidateQueries([queryKey, columnId]);
 };
 
-//Compares subtasks array from initialValue with subtasks from data submitted
-//Returns an array of subtasks ids to be deleted
-function getSubtasksToBeDeleted(
-	taskData: Partial<TaskSubmit>,
-	newTaskData: Partial<TaskSubmit>
-) {
-	if (newTaskData.subtasks) {
-		const dataSubtaskIds = new Set(
-			newTaskData.subtasks.map((subtask: SubtaskInput) => subtask.subtask_id)
-		);
-		if (taskData.subtasks) {
-			const subtasksToBeDeleted = taskData.subtasks
-				.filter(
-					(subtask: SubtaskInput) => !dataSubtaskIds.has(subtask.subtask_id)
-				)
-				.map((subtask: SubtaskInput) => subtask.subtask_id);
-
-			return subtasksToBeDeleted;
+const getEditedSubtask = (
+	subtasks: SingleSubtask[],
+	newSubtasks: SubtaskInput[]
+) => {
+	const subtasksToEdit = [];
+	if (subtasks.length === newSubtasks.length) {
+		for (let i = 0; i < subtasks.length; i++) {
+			if (
+				subtasks[i].title !== newSubtasks[i].title &&
+				!newSubtasks[i].is_new
+			) {
+				subtasksToEdit.push(newSubtasks[i]);
+			}
+		}
+	} else {
+		for (let i = 0; i < subtasks.length; i++) {
+			for (let j = 0; j < newSubtasks.length; j++) {
+				if (subtasks[i].subtask_id === newSubtasks[j].subtask_id) {
+					if (
+						subtasks[i].title !== newSubtasks[j].title &&
+						!newSubtasks[i].is_new
+					) {
+						subtasksToEdit.push(newSubtasks[j]);
+					}
+				}
+			}
 		}
 	}
-}
+	return subtasksToEdit;
+};
 
-// function getPropertiesToPatch
+const getNewSubtasksToAdd = (newSubtasks: SubtaskInput[]) => {
+	const newSubtasksToAdd = newSubtasks.filter(
+		(subtask: SubtaskInput) => subtask.is_new
+	);
+
+	return newSubtasksToAdd;
+};
+
+function compareSubtasks(
+	subtasks: SingleSubtask[],
+	newSubtasks: SubtaskInput[]
+) {
+	if (subtasks.length !== newSubtasks.length) {
+		return true;
+	}
+
+	for (let i = 0; i < subtasks.length; i++) {
+		if (
+			subtasks[i].subtask_id !== newSubtasks[i].subtask_id ||
+			subtasks[i].title !== newSubtasks[i].title
+		) {
+			return true;
+		}
+	}
+
+	return false;
+}
