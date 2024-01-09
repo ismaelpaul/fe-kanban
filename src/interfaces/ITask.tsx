b54871b79b9318347e4adf317d4bbccfab47e288
ismaelpaul <ismaafp@gmail.com>
2024-01-09 20:42:00 +0000

    Add TaskStatusDropdown and UpdatedTaskStatus interface

--- src/interfaces/ITask.tsx
+++ src/interfaces/ITask.tsx
@@ -1,4 +1,4 @@
-import { SubtaskSubmit } from './ISubtask';
+import { SubtaskInput } from './ISubtask';
 
 export interface SingleTask {
 	column_id: number;
@@ -14,9 +14,24 @@ export interface ITasks {
 }
 
 export interface TaskSubmit {
+	task_id?: number;
 	column_id: number;
 	title: string;
 	description: string;
 	status: string;
-	subtasks: SubtaskSubmit[];
+	position?: number;
+	subtasks: SubtaskInput[];
+}
+
+export interface TaskStatusDropdown {
+	id: number;
+	label: string;
+	value: string;
+}
+
+export interface UpdatedTaskStatus {
+	prevStatus: string | undefined;
+	newStatus: string;
+	newColumnId: number;
+	taskId: number;
 }
