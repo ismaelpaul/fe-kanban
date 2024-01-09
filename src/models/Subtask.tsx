a00c08081de0c7fc5e271f79c264a629d06463ca
ismaelpaul <ismaafp@gmail.com>
2024-01-09 20:40:06 +0000

    Remove optional method from subtask_id

--- src/models/Subtask.tsx
+++ src/models/Subtask.tsx
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const SubtaskSchema = z.object({
-	subtask_id: z.number().optional(),
+	subtask_id: z.number(),
 	title: z.string().nonempty({ message: "Can't be empty" }),
 	is_completed: z.boolean().default(false),
 	placeholder: z.string().optional(),
