b4a1b9e276aac174d8886aa69c2697a3aea09b1d
ismaelpaul <ismaafp@gmail.com>
2024-01-12 19:35:00 +0000

    Typo

--- src/store/userStore.tsx
+++ src/store/userStore.tsx
@@ -1,9 +1,9 @@
 import { create } from 'zustand';
 import { UserStoreActions, UserStoreState } from '../interfaces/IUserStore';
 
-const userStore = create<UserStoreState & UserStoreActions>((set) => ({
+const useUserStore = create<UserStoreState & UserStoreActions>((set) => ({
 	userId: 1,
 	setUserId: (newUserId: number) => set({ userId: newUserId }),
 }));
 
-export default userStore;
+export default useUserStore;
