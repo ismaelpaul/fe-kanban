d931b0071c0f6eaddf88220eccec435b59f05b35
ismaelpaul <ismaafp@gmail.com>
2024-01-13 20:14:30 +0000

    Change selectedBoard state to have all properties from board object

--- src/store/boardStore.tsx
+++ src/store/boardStore.tsx
@@ -1,11 +1,12 @@
 import { create } from 'zustand';
 import { BoardStoreActions, BoardStoreState } from '../interfaces/IBoardStore';
+import { Board } from '../interfaces/IBoard';
 
 const useBoardStore = create<BoardStoreState & BoardStoreActions>((set) => ({
 	boardId: 1,
 	setBoardId: (newBoardId: number) => set({ boardId: newBoardId }),
-	selectedBoard: '',
-	setSelectedBoard: (newSelectedBoard: string) =>
+	selectedBoard: { user_id: 0, board_id: 0, name: '' },
+	setSelectedBoard: (newSelectedBoard: Board) =>
 		set({ selectedBoard: newSelectedBoard }),
 }));
 
