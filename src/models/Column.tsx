4bea37df2dc3a220171daa42844e968fe025b30e
ismaelpaul <ismaafp@gmail.com>
2024-01-18 19:59:04 +0000

    Change column_name property for name and add ColumnSubmit schema

--- src/models/Column.tsx
+++ src/models/Column.tsx
@@ -1,6 +1,12 @@
 import { z } from 'zod';
 
 export const ColumnSchema = z.object({
-	column_id: z.number().optional(),
-	column_name: z.string().max(50).nonempty({ message: "Can't be empty" }),
+	column_id: z.number(),
+	name: z.string().max(50).nonempty({ message: "Can't be empty" }),
+	placeholder: z.string().optional(),
+	is_new: z.boolean().optional(),
+});
+
+export const ColumnSubmit = z.object({
+	name: z.string().max(50).nonempty({ message: "Can't be empty" }),
 });
