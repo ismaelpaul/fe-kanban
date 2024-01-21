8b770d583dbd741af56fe0a23b86a83f5d23b873
ismaelpaul <ismaafp@gmail.com>
2024-01-21 17:29:11 +0000

    Add toastReducer

---
+++ src/reducer/toastReducer.tsx
@@ -0,0 +1,42 @@
+import { Reducer } from 'react';
+import { IToast } from '../interfaces/IToast';
+
+interface ToastState {
+	toasts: IToast[];
+}
+
+interface AddToastAction {
+	type: 'ADD_TOAST';
+	payload: IToast;
+}
+
+interface DeleteToastAction {
+	type: 'DELETE_TOAST';
+	payload: number;
+}
+
+type ToastAction = AddToastAction | DeleteToastAction;
+
+export const toastReducer: Reducer<ToastState, ToastAction> = (
+	state,
+	action
+) => {
+	let updatedToasts;
+	switch (action.type) {
+		case 'ADD_TOAST':
+			return {
+				...state,
+				toasts: [...state.toasts, action.payload],
+			};
+		case 'DELETE_TOAST':
+			updatedToasts = state.toasts.filter(
+				(toast) => toast.id !== action.payload
+			);
+			return {
+				...state,
+				toasts: updatedToasts,
+			};
+		default:
+			throw new Error(`Unhandled action type: ${action.type}`);
+	}
+};
