d1fbc71ef91ddf05ab4c41fe9e3a60dac9a1c403
ismaelpaul <ismaafp@gmail.com>
2024-01-21 17:30:42 +0000

    Wrap ToastContextProvider in App component

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ToastContextProvider } from './contexts/ToastContext.tsx';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<QueryClientProvider client={queryClient}>
-		<App />
+		<ToastContextProvider>
+			<App />
+		</ToastContextProvider>
 		<ReactQueryDevtools initialIsOpen={true} />
 	</QueryClientProvider>
 );
