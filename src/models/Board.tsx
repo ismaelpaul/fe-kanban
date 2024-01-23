b7b07a118807cdc7f2fcfddb2c84b2e89d89e467
ismaelpaul <ismaafp@gmail.com>
2024-01-23 20:00:46 +0000

    Add boardSubmitSchema

--- src/models/Board.tsx
+++ src/models/Board.tsx
@@ -3,6 +3,9 @@ import { ColumnSchema } from './Column';
 
 export const BoardSchema = z.object({
 	board_id: z.number().optional(),
-	board_name: z.string().max(50).nonempty({ message: "Can't be empty" }),
+	name: z.string().max(50).nonempty({ message: "Can't be empty" }),
+});
+
+export const BoardSubmitSchema = BoardSchema.extend({
 	columns: z.array(ColumnSchema),
 });
