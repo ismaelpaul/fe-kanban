db574f1ac8e7e1c27f45fbcab521540919754b0e
ismaelpaul <ismaafp@gmail.com>
2024-02-04 16:56:16 +0000

    Add useFetchSubtasks custom hook

--- src/hooks/useFetchSubtasks.ts
+++ src/hooks/useFetchSubtasks.ts
@@ -1,15 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
 import { getSubtasksByTaskId } from '../api/kanbanApi';
 import { ISubtasks } from '../interfaces/ISubtask';
+import { useEffect } from 'react';
 
 const useFetchSubtasks = (taskId: number) => {
 	const subtaskQueryKey = `subtasks_${taskId}`;
 
-	const { data, isLoading, isError } = useQuery([subtaskQueryKey], () =>
-		getSubtasksByTaskId(taskId)
+	const { data, isLoading, isError, refetch } = useQuery(
+		[subtaskQueryKey],
+		() => getSubtasksByTaskId(taskId),
+		{
+			enabled: false,
+		}
 	);
 
 	const { subtasks } = (data as ISubtasks) || { subtasks: [] };
+
+	useEffect(() => {
+		refetch();
+	}, []);
 
 	return { subtasks, isLoading, isError };
 };
