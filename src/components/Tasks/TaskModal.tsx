786be6c14e2e1aae99652ff932cec8ddb0ad1421
ismaelpaul <ismaafp@gmail.com>
2024-02-11 17:21:48 +0000

    Refactor TaskModal component
    The refactoring was done with Single Responsability Principle in mind

--- src/components/Tasks/TaskModal.tsx
+++ src/components/Tasks/TaskModal.tsx
@@ -1,12 +1,8 @@
 import { useRef, useState } from 'react';
-import {
-	deleteTaskById,
-	updateSubtaskCompletionById,
-} from '../../api/kanbanApi';
+import { deleteTaskById } from '../../api/kanbanApi';
 import { SingleSubtask } from '../../interfaces/ISubtask';
 import { SingleTask } from '../../interfaces/ITask';
 import Dropdown from '../Dropdown/Dropdown';
-import CheckIcon from '../SVGComponents/CheckIcon';
 import KebabMenu from '../SVGComponents/KebabMenu';
 import useClickOutside from '../../hooks/useClickOutside';
 import usePatch from '../../hooks/usePatch';
@@ -17,6 +13,9 @@ import { useQueryClient } from '@tanstack/react-query';
 import useBoardStore from '../../store/boardStore';
 import { IColumns, SingleColumn } from '../../interfaces/IColumn';
 import { motion } from 'framer-motion';
+import useKebabMenu from '../../hooks/useKebabMenu';
+import SubtasksList from '../Subtasks/SubtaskList';
+import { handleSubtaskCompletion } from '../../utils/Subtask/SubtaskUtils';
 
 interface TaskModalProps {
 	task: SingleTask;
@@ -39,9 +38,10 @@ const TaskModal = ({
 	columnId,
 	setIsEditTaskModalOpen,
 }: TaskModalProps) => {
-	const [isKebabModalOpen, setIsKebabModalOpen] = useState(false);
 	const [updatingSubtask, setUpdatingSubtask] = useState<number | null>(null);
 	const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+	const { isKebabMenuOpen, handleKebabMenu } = useKebabMenu();
 
 	const boardId = useBoardStore((state) => state.boardId);
 
@@ -80,44 +80,19 @@ const TaskModal = ({
 		}
 	});
 
-	const handleKebabMenu = () => {
-		setIsKebabModalOpen(!isKebabModalOpen);
-	};
-
-	const handleSubtaskCompletion = async (
+	const handleSubtaskToggle = async (
 		subtaskId: number,
 		isCompleted: boolean
 	) => {
-		if (updatingSubtask === subtaskId) {
-			isCompleted = !isCompleted;
-
-			const newCompleted = isCompleted
-				? completedSubtasks + 1
-				: completedSubtasks - 1;
-
-			setCompletedSubtasks(newCompleted);
-			setUpdatingSubtask(null);
-		} else {
-			setUpdatingSubtask(subtaskId);
-		}
-
-		const newCompleted = isCompleted
-			? completedSubtasks - 1
-			: completedSubtasks + 1;
-
-		setCompletedSubtasks(newCompleted);
-
-		const updatedData = {
-			is_completed: !isCompleted,
-		};
-		const queryKey = 'subtasks';
-
-		await patch({
-			patchFn: updateSubtaskCompletionById,
-			resourceId: subtaskId,
-			updatedData,
-			queryKey,
-		});
+		await handleSubtaskCompletion(
+			subtaskId,
+			isCompleted,
+			updatingSubtask,
+			completedSubtasks,
+			setUpdatingSubtask,
+			setCompletedSubtasks,
+			patch
+		);
 	};
 
 	const handleDeleteTask = async (taskId: number) => {
@@ -137,7 +112,7 @@ const TaskModal = ({
 					initial={{ scale: 0.7 }}
 					animate={{ scale: 1 }}
 					transition={{ duration: 0.2 }}
-					className="bg-white dark:bg-dark-grey p-6 rounded-md z-50 mx-auto w-screen tablet:w-[30rem]"
+					className="bg-white dark:bg-dark-grey p-6 rounded-md z-50 mx-auto w-screen tablet:w-[30rem] max-h-[80vh] overflow-y-scroll no-scrollbar"
 					ref={modalRef}
 					role="dialog"
 					aria-labelledby="modal-heading"
@@ -150,13 +125,12 @@ const TaskModal = ({
 							<KebabMenu />
 						</div>
 					</div>
-					{isKebabModalOpen ? (
+					{isKebabMenuOpen ? (
 						<KebabMenuModal
 							editText={kebabMenuEdit}
 							deleteText={kebabMenuDelete}
 							menuPosition={kebabMenuPosition}
 							setIsDeleteModalOpen={setIsDeleteModalOpen}
-							setIsKebabMenuOpen={setIsKebabModalOpen}
 							setIsTaskModalOpen={setIsTaskModalOpen}
 							isParentTaskModal={true}
 							setIsEditTaskModalOpen={setIsEditTaskModalOpen}
@@ -167,42 +141,12 @@ const TaskModal = ({
 					<p className="text-l-body text-medium-grey mt-6">
 						{task.description}
 					</p>
-					<span className="text-12px text-medium-grey dark:text-white font-bold inline-block mt-6 mb-2">
-						Subtasks ({completedSubtasks} of {totalSubtasks})
-					</span>
-					{subtasks.map((subtask: SingleSubtask) => {
-						const isCompleted = subtask.is_completed === true;
-						return (
-							<div
-								key={subtask.subtask_id}
-								className="flex items-center bg-lines-light dark:bg-dark-bg p-3 mt-2 rounded gap-4 transition ease-in-out duration-200 hover:bg-purple/25"
-							>
-								<input
-									type="checkbox"
-									id={subtask.subtask_id.toString()}
-									defaultChecked={isCompleted}
-									onChange={() => {
-										handleSubtaskCompletion(
-											subtask.subtask_id,
-											subtask.is_completed
-										);
-									}}
-									className="relative peer appearance-none w-4 h-4 bg-white border border-medium-grey border-opacity-25 rounded-sm checked:bg-purple checked:border-0 cursor-pointer"
-								/>
-								<label
-									htmlFor={subtask.subtask_id.toString()}
-									className="text-12px dark:text-white font-bold peer-checked:opacity-50 peer-checked:line-through cursor-pointer"
-								>
-									{subtask.title}
-								</label>
-								<CheckIcon
-									className={
-										'absolute w-4 h-4 mt-2 ml-[3px] hidden peer-checked:block pointer-events-none'
-									}
-								/>
-							</div>
-						);
-					})}
+					<SubtasksList
+						completedSubtasks={completedSubtasks}
+						totalSubtasks={totalSubtasks}
+						subtasks={subtasks}
+						handleSubtaskToggle={handleSubtaskToggle}
+					/>
 					<div className="mt-6">
 						<span className="text-12px text-medium-grey dark:text-white font-bold inline-block mb-2">
 							Current Status
