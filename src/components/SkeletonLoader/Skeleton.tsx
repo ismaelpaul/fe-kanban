bb4bd664b6fe93be33be068cfeb38c599e270b00
ismaelpaul <ismaafp@gmail.com>
2024-02-20 20:35:50 +0000

    Add Skeleton component

---
+++ src/components/SkeletonLoader/Skeleton.tsx
@@ -0,0 +1,10 @@
+type SkeletonProps = {
+	classes: string;
+};
+
+const Skeleton = ({ classes }: SkeletonProps) => {
+	const classNames = `skeleton ${classes} animate-pulse`;
+
+	return <div className={classNames}></div>;
+};
+export default Skeleton;
