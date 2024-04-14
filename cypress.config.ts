ce66ba62e2d37155cae70b11b6491efa73a187a9
ismaelpaul <ismaafp@gmail.com>
2024-04-14 18:32:21 +0100

    Add env variables

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'cypress';
+import 'dotenv/config';
 
 export default defineConfig({
 	e2e: {
-		baseUrl: 'http://localhost:5173/',
+		baseUrl: 'http://localhost:5173/boards',
+	},
+	env: {
+		googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+		googleClientId: process.env.VITE_GOOGLE_CLIENT_ID,
+		googleClientSecret: process.env.VITE_GOOGLE_CLIENT_SECRET,
 	},
 });
