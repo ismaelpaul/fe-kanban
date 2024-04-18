c83c118b65c8ad9f2a0120e60e6acfae1077faeb
ismaelpaul <ismaafp@gmail.com>
2024-04-18 19:57:28 +0100

    Import LoginForm component

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,76 +1,62 @@
-import Button from '../../components/Button/Button';
 import Card from '../../components/Card/Card';
 import { Link } from 'react-router-dom';
 import GoogleIcon from '../../components/SVGComponents/GoogleIcon';
 import GithubIcon from '../../components/SVGComponents/GithubIcon';
+import LoginForm from '../../components/Form/LoginForm';
 
 const Login = () => {
 	const cardClass =
 		'bg-white drop-shadow-card px-4 py-6 rounded-lg max-w-[25%]';
-	const inputClass =
-		' border border-medium-grey border-opacity-25 rounded px-4 py-2 text-l-body w-full cursor-pointer hover:border-purple focus:outline-none';
-	const labelClass = 'text-body text-medium-grey font-bold';
 
-	const btnLoginClass =
-		'bg-purple text-white text-13px font-bold py-2 w-full rounded-full';
+	const btnClass =
+		'text-white text-13px py-2 w-full rounded-full transition ease-in duration-200';
 
-	const google = () => {
+	const googleAuth = () => {
 		window.open('http://localhost:9090/auth/google', '_self');
 	};
+	const githubAuth = () => {
+		window.open('http://localhost:9090/auth/github', '_self');
+	};
 	return (
-		<div className="min-h-screen bg-dark-grey flex items-center justify-center">
+		<div className="min-h-screen bg-dark-grey flex flex-col items-center justify-center">
 			<Card cardClass={cardClass}>
 				<>
 					<h1>Choose a Login Method</h1>
 					<div>
-						<div>
-							<label htmlFor="email" className={labelClass}>
-								Email
-							</label>
-							<input type="text" placeholder="Email" className={inputClass} />
-							<label htmlFor="password" className={labelClass}>
-								Password
-							</label>
-							<input
-								type="text"
-								placeholder="Password"
-								className={inputClass}
-							/>
-							<Button buttonText={'Login'} buttonClass={btnLoginClass} />
-						</div>
+						<LoginForm />
 						<div className="flex items-center gap-2 my-4">
 							<hr className="h-px w-full bg-lines-light border-0" />
-							<span className="px-2">or</span>
+							<span className="px-2 text-13px text-medium-grey">or</span>
 							<hr className="h-px w-full bg-lines-light border-0" />
 						</div>
 						<div className="flex flex-col gap-4">
 							<button
-								onClick={google}
+								onClick={googleAuth}
 								type="button"
-								className="py-2 px-4 flex justify-center items-center  bg-google-bg hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none rounded-lg "
+								className={`${btnClass} flex justify-center	 bg-google-bg hover:bg-google-bg/80`}
 							>
 								<GoogleIcon />
 								Sign in with Google
 							</button>
 
 							<button
+								onClick={githubAuth}
 								type="button"
-								className="py-2 px-4 flex justify-center items-center  bg-github-bg hover:bg-gray-700 focus:ring-gray-500 focus:ring-offset-gray-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none rounded-lg "
+								className={`${btnClass} flex justify-center	 bg-github-bg hover:bg-github-bg/80`}
 							>
 								<GithubIcon />
 								Sign in with GitHub
 							</button>
 						</div>
-						<hr className="h-px my-4 bg-lines-light border-0" />
-						<span>
-							Don't have an account yet?
-							<Link to="/register">
-								<strong> Register</strong>
-							</Link>
-						</span>
 					</div>
 				</>
 			</Card>
+			<span className="text-13px text-white">
+				Don't have an account yet?
+				<Link to="/register">
+					<strong> Register</strong>
+				</Link>
+			</span>
 		</div>
 	);
 };
