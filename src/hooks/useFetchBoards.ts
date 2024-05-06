f972f124fadbe7ca19609e536345544e46271359
ismaelpaul <ismaafp@gmail.com>
2024-05-06 17:01:12 +0100

    Remove refetch

--- src/hooks/useFetchBoards.ts
+++ src/hooks/useFetchBoards.ts
@@ -1,26 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { getAllBoards } from '../api/kanbanApi';
 import { Boards } from '../interfaces/IBoard';
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const useFetchBoards = () => {
 	const navigate = useNavigate();
 	const queryKey = ['boards'];
 
-	const { data, isLoading, isError, refetch } = useQuery(
-		queryKey,
-		getAllBoards,
-		{
-			enabled: false,
-		}
-	);
+	const { data, isLoading, isError } = useQuery(queryKey, getAllBoards);
 
 	const { boards } = (data as Boards) || { boards: [] };
-
-	useEffect(() => {
-		refetch();
-	}, []);
 
 	if (isError) navigate('/login');
 
