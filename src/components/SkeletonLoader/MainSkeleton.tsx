162803371f2293b821233d43c7b9b74f795523ea
ismaelpaul <ismaafp@gmail.com>
2024-05-07 20:56:05 +0100

    Add MainSkeleton component

---
+++ src/components/SkeletonLoader/MainSkeleton.tsx
@@ -0,0 +1,13 @@
+import ColumnsSkeleton from './ColumnsSkeleton';
+import NavSkeleton from './NavSkeleton';
+
+const MainSkeleton = () => {
+	return (
+		<>
+			<NavSkeleton />
+			<ColumnsSkeleton />
+		</>
+	);
+};
+
+export default MainSkeleton;
