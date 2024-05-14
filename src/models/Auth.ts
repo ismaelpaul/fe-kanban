9bca7d523238cf5ac08ae0eb585422d0e090a2aa
ismaelpaul <ismaafp@gmail.com>
2024-05-14 21:07:30 +0100

    Update Login and Register schemas

--- src/models/Auth.ts
+++ src/models/Auth.ts
@@ -1,19 +1,39 @@
 import { z } from 'zod';
+import { emailExists } from '../api/kanbanApi';
 
 export const LoginSchema = z.object({
-	email: z.string().min(1, { message: "Can't be empty." }),
-	// TODO:
-	// .email('This is not a valid email.').refine(async (e) => {
-	//     // make a request to the backend
-	//     // to see if the email is valid.
-	//     return await checkIfEmailIsValid(e);
-	//   }, "This email is not in our database")
+	email: z
+		.string()
+		.min(1, { message: "Can't be empty." })
+		.email('This is not a valid email.')
+		.refine(async (email) => {
+			return await emailExists(email);
+		}, 'This email is not registered'),
 	password: z.string(),
 });
 
-export const RegisterSchema = z.object({
-	firstName: z.string(),
-	lastName: z.string(),
-	email: z.string(),
-	password: z.string(),
-});
+export const RegisterSchema = z
+	.object({
+		firstName: z
+			.string()
+			.min(2, { message: 'First name must contain at least 2 characters' }),
+		lastName: z
+			.string()
+			.min(2, { message: 'Last name must contain at least 2 characters' }),
+		email: z
+			.string()
+			.min(2, { message: 'Email must contain at least 2 characters' })
+			.email('This is not a valid email.')
+			.refine(async (email) => {
+				const exists = await emailExists(email);
+				if (exists) {
+					return 'This email is already in use';
+				}
+			}, 'This email is not in our database'),
+		password: z.string(),
+		confirmPassword: z.string(),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Passwords don't match",
+		path: ['confirmPassword'],
+	});
