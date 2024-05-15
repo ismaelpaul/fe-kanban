6457d611e82e070f413d3cb659ef1778c75a4c5f
ismaelpaul <ismaafp@gmail.com>
2024-05-15 20:46:28 +0100

    Update Login and Register schemas

--- src/models/Auth.ts
+++ src/models/Auth.ts
@@ -4,12 +4,15 @@ import { emailExists } from '../api/kanbanApi';
 export const LoginSchema = z.object({
 	email: z
 		.string()
-		.min(1, { message: "Can't be empty." })
+		.min(2, { message: "Can't be empty." })
 		.email('This is not a valid email.')
 		.refine(async (email) => {
 			return await emailExists(email);
 		}, 'This email is not registered'),
-	password: z.string(),
+	password: z
+		.string()
+		.min(6, { message: 'Password has at least 6 characters' })
+		.nonempty({ message: "Can't be empty" }),
 });
 
 export const RegisterSchema = z
@@ -23,15 +26,12 @@ export const RegisterSchema = z
 		email: z
 			.string()
 			.min(2, { message: 'Email must contain at least 2 characters' })
-			.email('This is not a valid email.')
-			.refine(async (email) => {
-				const exists = await emailExists(email);
-				if (exists) {
-					return 'This email is already in use';
-				}
-			}, 'This email is not in our database'),
-		password: z.string(),
-		confirmPassword: z.string(),
+			.email('Please enter a valid email.'),
+		password: z
+			.string()
+			.min(6, { message: 'Password must contain at least 6 characters' })
+			.nonempty({ message: "Can't be empty" }),
+		confirmPassword: z.string().min(6).nonempty({ message: "Can't be empty" }),
 	})
 	.refine((data) => data.password === data.confirmPassword, {
 		message: "Passwords don't match",
