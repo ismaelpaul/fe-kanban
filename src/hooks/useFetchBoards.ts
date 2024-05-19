bef981033a83b9526db35fb00860927a89eff8e0
ismaelpaul <ismaafp@gmail.com>
2024-05-19 14:43:56 +0100

    Update hook to handle errors with toast

--- src/hooks/useFetchBoards.ts
+++ src/hooks/useFetchBoards.ts
@@ -2,16 +2,23 @@ import { useQuery } from '@tanstack/react-query';
 import { getAllBoards } from '../api/kanbanApi';
 import { Boards } from '../interfaces/IBoard';
 import { useNavigate } from 'react-router-dom';
+import { useToast } from './useToast';
 
 const useFetchBoards = () => {
 	const navigate = useNavigate();
+	const toast = useToast();
 	const queryKey = ['boards'];
 
-	const { data, isLoading, isError } = useQuery(queryKey, getAllBoards);
+	const { data, isLoading, isError } = useQuery(queryKey, getAllBoards, {
+		onError: (err) => {
+			if (err instanceof Error) {
+				toast.error(err.message);
+			}
+			navigate('/login');
+		},
+	});
 
 	const { boards } = (data as Boards) || { boards: [] };
-
-	if (isError) navigate('/login');
 
 	return { boards, isLoading, isError };
 };
