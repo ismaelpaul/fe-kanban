819774b2d733af172995c62d80e81cf4f39ddbf7
ismaelpaul <ismaafp@gmail.com>
2024-05-26 17:45:04 +0100

    Remove boardId because it was redundant

--- src/interfaces/IBoardStore.ts
+++ src/interfaces/IBoardStore.ts
@@ -1,11 +1,9 @@
 import { Board } from './IBoard';
 
 export interface BoardStoreState {
-	boardId: number;
 	selectedBoard: Board;
 }
 
 export interface BoardStoreActions {
-	setBoardId: (newBoardId: number) => void;
 	setSelectedBoard: (newSelectedBoard: Board) => void;
 }
