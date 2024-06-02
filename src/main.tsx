284a486f9dc72e7506c00285d0e974ac028c03f0
ismaelpaul <ismaafp@gmail.com>
2024-06-02 15:22:12 +0100

    Update queryClient to not refetch data on window focus

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,14 @@ import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ToastContextProvider } from './contexts/ToastContext.tsx';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			cacheTime: 1000 * 60 * 5,
+		},
+	},
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<QueryClientProvider client={queryClient}>
