8e617e15d8fff48c4221f5a04282835625e6281a
ismaelpaul <ismaafp@gmail.com>
2024-06-02 17:34:09 +0100

    Pass tasks down as props

--- src/components/Columns/ColumnHeader.tsx
+++ src/components/Columns/ColumnHeader.tsx
@@ -1,19 +1,22 @@
 import { SingleColumn } from '../../interfaces/IColumn';
+import { SingleTask } from '../../interfaces/ITask';
 
 interface ColumnHeaderPros {
 	dotColor: string;
-	tasksLength: number;
 	column: SingleColumn;
+	tasks: { [columnId: number]: SingleTask[] };
 }
 
-const ColumnHeader = ({ dotColor, tasksLength, column }: ColumnHeaderPros) => {
+const ColumnHeader = ({ dotColor, column, tasks }: ColumnHeaderPros) => {
+	const tasksCount = (tasks[column.column_id] || []).length;
+
 	return (
 		<div className="flex gap-3 items-center mb-7">
 			<div
 				className={` ${dotColor} w-[0.938rem] h-[0.938rem] rounded-full`}
 			></div>
 			<h1 className="text-s-heading text-medium-grey tracking-2.4px">
-				{column.name.toUpperCase()} ({tasksLength})
+				{column.name.toUpperCase()} ({tasksCount})
 			</h1>
 		</div>
 	);
