65883f496937e2f75077790b82aa2647252a3c5b
ismaelpaul <ismaafp@gmail.com>
2024-06-13 20:11:30 +0100

    Add GoogleButton component

---
+++ src/components/Button/GoogleButton.tsx
@@ -0,0 +1,25 @@
+import GoogleIcon from '../SVGComponents/GoogleIcon';
+
+const GoogleButton = () => {
+	const authUrl = import.meta.env.VITE_AUTH_URL;
+
+	const googleAuth = () => {
+		window.open(`${authUrl}/auth/google`, '_self');
+	};
+
+	const btnClass =
+		'text-white text-13px py-2 w-full rounded-full transition ease-in duration-200';
+
+	return (
+		<button
+			onClick={googleAuth}
+			type="button"
+			className={`${btnClass} flex justify-center	 bg-google-bg hover:bg-google-bg/80`}
+		>
+			<GoogleIcon />
+			Sign in with Google
+		</button>
+	);
+};
+
+export default GoogleButton;
