1c3ad7148869cb022e073a1a5e88946e239da4c8
ismaelpaul <ismaafp@gmail.com>
2024-06-18 21:02:38 +0100

    Update config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'cypress';
-import 'dotenv/config';
+import { defineConfig } from "cypress";
+import "dotenv/config";
 
 export default defineConfig({
-	e2e: {
-		baseUrl: 'http://localhost:5173/boards',
-	},
-	env: {
-		googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-		googleClientId: process.env.VITE_GOOGLE_CLIENT_ID,
-		googleClientSecret: process.env.VITE_GOOGLE_CLIENT_SECRET,
-	},
+  e2e: {
+    baseUrl: "http://localhost:5173",
+  },
+
+  env: {
+    googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    googleClientId: process.env.VITE_GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.VITE_GOOGLE_CLIENT_SECRET,
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
 });
