8ddcf62ba42d9e10f4ccf07bdd733115625b932c
ismaelpaul <ismaafp@gmail.com>
2024-12-27 15:47:48 +0000

    Rename extension

--- src/store/boards/boardStore.ts
+++ src/store/boards/boardStore.ts
@@ -4,7 +4,7 @@ import { Board } from '@/interfaces/IBoard';
 import { BoardStoreActions, BoardStoreState } from '@/interfaces/IBoardStore';
 
 const useBoardStore = create<BoardStoreState & BoardStoreActions>((set) => ({
-	selectedBoard: { user_id: 0, board_id: 0, name: '' },
+	selectedBoard: { board_id: 0, name: '' },
 	setSelectedBoard: (newSelectedBoard: Board) =>
 		set({ selectedBoard: newSelectedBoard }),
 }));
