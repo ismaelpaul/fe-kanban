b85893f6e8f6d4b705eca4a13a51bb2390719f8a
ismaelpaul <ismaafp@gmail.com>
2025-01-01 18:15:59 +0000

    Add taskEditHandler

---
+++ src/handlers/tasks/taskEditHandler.ts
@@ -0,0 +1,21 @@
+import { QueryClient } from '@tanstack/react-query';
+
+type TaskEditHandler = {
+	type: string;
+	newColumn_id: number | null;
+	column_id: number;
+};
+export const TaskEditHandler = (
+	data: TaskEditHandler,
+	queryClient: QueryClient
+) => {
+	const newColumnId = data.newColumn_id;
+	const columnId = data.column_id;
+
+	if (newColumnId) {
+		queryClient.invalidateQueries(['tasks', newColumnId]);
+		queryClient.invalidateQueries(['tasks', columnId]);
+	} else {
+		queryClient.invalidateQueries(['tasks', columnId]);
+	}
+};
