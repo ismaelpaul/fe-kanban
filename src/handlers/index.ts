6b38a1d05dad4737d39bf9e3a8bdae98b1dd96d5
ismaelpaul <ismaafp@gmail.com>
2025-01-01 18:16:46 +0000

    Add TASK_ADDED, TASK_DELETED and TASK_INFO_UPDATE handlers

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,9 +1,14 @@
 import { updateSubtaskCompletionHandler } from './subtasks/updateSubtaskCompletion';
+import { TaskAddedAndDeleteHandler } from './tasks/taskAddedHandler';
+import { TaskEditHandler } from './tasks/taskEditHandler';
 import { updateTaskCompletionHandler } from './tasks/updateTaskCompletion';
 
 const handlers = {
 	UPDATE_TASK_COMPLETION: updateTaskCompletionHandler,
 	TASK_COMPLETION_UPDATED: updateTaskCompletionHandler,
+	TASK_ADDED: TaskAddedAndDeleteHandler,
+	TASK_DELETED: TaskAddedAndDeleteHandler,
+	TASK_INFO_UPDATED: TaskEditHandler,
 	SUBTASK_COMPLETION_UPDATED: updateSubtaskCompletionHandler,
 };
 
