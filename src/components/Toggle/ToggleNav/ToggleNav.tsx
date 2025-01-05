fad0dc96f3599fb8b7d7915672e4024ad8e419a9
ismaelpaul <ismaafp@gmail.com>
2025-01-05 09:57:36 +0000

    Refactor ToggleNav using useModalStore

--- src/components/Toggle/ToggleNav/ToggleNav.tsx
+++ src/components/Toggle/ToggleNav/ToggleNav.tsx
@@ -1,18 +1,17 @@
+import { useModalStore } from '@/store/modals';
+
 import { ShowSidebarIcon } from '@/components/SVGComponents/ShowSidebarIcon';
 
-interface NavProps {
-	setIsSidebarNavOpen: (arg: boolean) => void;
-	isSidebarNavOpen: boolean;
-}
+const ToggleNav = () => {
+	const { modals, openModal } = useModalStore();
 
-const ToggleNav = ({ setIsSidebarNavOpen, isSidebarNavOpen }: NavProps) => {
 	return (
 		<aside
 			onClick={() => {
-				setIsSidebarNavOpen(true);
+				openModal('sidebarNav');
 			}}
 			className={`hidden transition-all ease-in-out duration-300 absolute ${
-				isSidebarNavOpen ? 'scale-0 -left-14' : 'scale-100 left-0'
+				modals.sidebar ? 'scale-0 -left-14' : 'scale-100 left-0'
 			} tablet:flex items-center justify-center bottom-12  bg-purple hover:bg-purple-hover w-14 h-12 rounded-r-full cursor-pointer`}
 		>
 			<ShowSidebarIcon />
