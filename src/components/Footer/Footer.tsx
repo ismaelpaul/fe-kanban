cf6f89340b744b400e0176fa2968a2904e569556
ismaelpaul <ismaafp@gmail.com>
2025-01-05 09:18:40 +0000

    Add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<footer className="flex items-center justify-center bg-white dark:bg-dark-grey text-black dark:text-white text-body h-[7vh]">
+			<p>Kanban 2025 </p>
+		</footer>
+	);
+};
+
+export { Footer };
