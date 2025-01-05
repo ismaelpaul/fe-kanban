b2e56eed04fc1f836904be22a4f058d327e3988a
ismaelpaul <ismaafp@gmail.com>
2025-01-05 09:22:07 +0000

    Add ToggleColorMode component

---
+++ src/components/Toggle/ToggleColorMode/ToggleColorMode.tsx
@@ -0,0 +1,15 @@
+import { DarkTheme } from '@/components/SVGComponents/DarkTheme';
+import { LightTheme } from '@/components/SVGComponents/LightTheme';
+import { SwitchButton } from '@/components/SwitchButton';
+
+const ToggleColorMode = () => {
+	return (
+		<div className="bg-light-bg dark:bg-dark-bg flex items-center justify-center gap-6 h-12 rounded-md mt-4 mx-4">
+			<LightTheme />
+			<SwitchButton />
+			<DarkTheme />
+		</div>
+	);
+};
+
+export { ToggleColorMode };
