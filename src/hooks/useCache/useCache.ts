64ceeb39e6a5d74493259beb291b76cb8cd6a49e
ismaelpaul <ismaafp@gmail.com>
2025-01-06 20:11:40 +0000

    Update useCache hook to make number as optional

--- src/hooks/useCache/useCache.ts
+++ src/hooks/useCache/useCache.ts
@@ -1,6 +1,7 @@
 import { useQueryClient } from '@tanstack/react-query';
+type CacheKey = [string, number?];
 
-const useCache = (key: (string | number)[]) => {
+const useCache = (key: CacheKey) => {
 	const queryClient = useQueryClient();
 	const cachedData = queryClient.getQueryData(key);
 
