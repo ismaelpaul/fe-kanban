75e62fc6e8c790183245493532e6c8ee5b2480a6
ismaelpaul <ismaafp@gmail.com>
2025-01-08 19:32:48 +0000

    Add isHovering state

--- src/components/Boards/CreateNewBoard/CreateNewBoard.tsx
+++ src/components/Boards/CreateNewBoard/CreateNewBoard.tsx
@@ -1,7 +1,9 @@
 import { BoardIcon } from '@/components/SVGComponents/BoardIcon';
 import { useModalStore } from '@/store/modals';
+import { useState } from 'react';
 
 const CreateNewBoard = () => {
+	const [isHovering, setIsHovering] = useState(false);
 	const { openModal, closeModal } = useModalStore();
 
 	const handleAddNewBoardModal = () => {
@@ -10,8 +12,14 @@ const CreateNewBoard = () => {
 	};
 
 	return (
-		<div className="flex items-center gap-3 h-12 text-m-heading pl-6 cursor-pointer text-purple">
-			<BoardIcon iconClass={'fill-purple'} />
+		<div
+			className="flex items-center gap-3 h-12 text-m-heading pl-6 cursor-pointer text-purple hover:text-medium-grey"
+			onMouseEnter={() => setIsHovering(true)}
+			onMouseLeave={() => setIsHovering(false)}
+		>
+			<BoardIcon
+				iconClass={`${isHovering ? 'fill-medium-grey' : 'fill-purple'}`}
+			/>
 			<span onClick={handleAddNewBoardModal}> + Create New Board</span>
 		</div>
 	);
