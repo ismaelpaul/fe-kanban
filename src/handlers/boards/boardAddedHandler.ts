382ad8dd427e3cdc0115ae7316c14785bc798e46
ismaelpaul <ismaafp@gmail.com>
2025-01-08 19:36:02 +0000

    Remove setSelectedBoard

--- src/handlers/boards/boardAddedHandler.ts
+++ src/handlers/boards/boardAddedHandler.ts
@@ -1,5 +1,4 @@
 import { Board } from '@/interfaces/IBoard';
-import { useBoardStore } from '@/store/boards';
 import { QueryClient } from '@tanstack/react-query';
 
 type BoardAddedHandler = {
@@ -11,12 +10,6 @@ export const boardAddedHandler = (
 	data: BoardAddedHandler,
 	queryClient: QueryClient
 ) => {
-	const { setSelectedBoard } = useBoardStore.getState();
-
-	const newBoard = data.board;
-
-	setSelectedBoard(newBoard);
-
 	const teamId = data.board.team_id;
 
 	queryClient.invalidateQueries(['boards', teamId]);
