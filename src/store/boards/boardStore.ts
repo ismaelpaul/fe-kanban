d8cbaf1340d7565757fab7eacbd57d330767fdfc
ismaelpaul <ismaafp@gmail.com>
2025-01-08 19:37:05 +0000

    Add team_id to selectedBoard state

--- src/store/boards/boardStore.ts
+++ src/store/boards/boardStore.ts
@@ -4,7 +4,7 @@ import { Board } from '@/interfaces/IBoard';
 import { BoardStoreActions, BoardStoreState } from '@/interfaces/IBoardStore';
 
 const useBoardStore = create<BoardStoreState & BoardStoreActions>((set) => ({
-	selectedBoard: { board_id: 0, name: '' },
+	selectedBoard: { board_id: 0, name: '', team_id: 0 },
 	setSelectedBoard: (newSelectedBoard: Board) =>
 		set({ selectedBoard: newSelectedBoard }),
 }));
