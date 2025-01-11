461a2eab153503e09c16e2bc16fcfe9d069699d6
ismaelpaul <ismaafp@gmail.com>
2025-01-11 09:54:26 +0000

    Add a check to make sure data is defined
    Also returning null in case data is not defined. Returning an empty array was causing the app to crash in case data was not defined yet

--- src/hooks/useFetchUser/useFetchUser.ts
+++ src/hooks/useFetchUser/useFetchUser.ts
@@ -2,14 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getUser } from '@/api/kanbanApi';
 
-import { User } from '@/interfaces/IUser';
+import { UserInfo } from '@/interfaces/IUser';
 
 const useFetchUser = () => {
 	const queryKey = ['user'];
 
 	const { data, isLoading, isError } = useQuery(queryKey, getUser);
 
-	const { user } = (data as User) || { user: [] };
+	const user = data ? (data.user as UserInfo) : null;
 
 	return { user, isLoading, isError };
 };
