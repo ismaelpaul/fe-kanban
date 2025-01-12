21864110f632497511a4313d09f7716d31bbd8be
ismaelpaul <ismaafp@gmail.com>
2025-01-12 18:48:03 +0000

    Rename extensions

--- src/models/Subtask.tsx
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const SubtaskSchema = z.object({
-	subtask_id: z.number(),
-	title: z.string().nonempty({ message: "Can't be empty" }),
-	is_completed: z.boolean().default(false),
-	placeholder: z.string().optional(),
-	is_new: z.boolean().optional(),
-});
