a66e8013cbb9c41145b117873d56579aba6a6a35
ismaelpaul <ismaafp@gmail.com>
2025-01-15 20:00:48 +0000

    Divide Dropdown component into options and actions

--- src/components/Dropdown/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown/Dropdown.tsx
@@ -1,36 +1,39 @@
-import React, { useState } from 'react';
-
-import { ChevronDown } from '@/components/SVGComponents/ChevronDown';
-import { ChevronUp } from '@/components/SVGComponents/ChevronUp';
+import { useState } from 'react';
 
 import { Options } from '@/interfaces/IOptionsDropdown';
 
-interface DropDownProps {
+import { ChevronDown } from '@/components/SVGComponents/ChevronDown';
+import { ChevronUp } from '@/components/SVGComponents/ChevronUp';
+import { DropdownActions } from '../DropdownActions';
+import { DropdownOptions } from '../DropdownOptions';
+
+export interface Action {
+	label: string;
+	onClick: () => void;
+}
+
+type DropDownProps = {
 	selectedOption: Options;
 	options: Options[];
-	setSelectedOption: (arg: Options) => void;
+	setSelectedOption?: (arg: Options) => void;
 	disabled?: boolean;
-}
+	actions: Action[];
+};
+
 const Dropdown = ({
 	selectedOption,
 	options,
 	setSelectedOption,
 	disabled,
+	actions,
 }: DropDownProps) => {
 	const [isOpen, setIsOpen] = useState(false);
-
-	const handleOptionClick = (option: Options) => {
-		if (!disabled) {
-			setSelectedOption(option);
-			setIsOpen(false);
-		}
-	};
 
 	return (
 		<div className="flex items-center gap-2 relative cursor-pointer mt-2">
 			<button
 				type="button"
-				className="flex items-center justify-between border border-medium-grey border-opacity-25 px-4 py-3 rounded w-full focus:border-purple"
+				className="flex items-center justify-between border border-medium-grey border-opacity-25 px-4 py-3 gap-2 rounded w-full focus:border-purple"
 				onClick={() => setIsOpen(!isOpen)}
 				disabled={disabled}
 			>
@@ -44,21 +47,19 @@ const Dropdown = ({
 				{isOpen ? <ChevronUp /> : <ChevronDown />}
 			</button>
 			{isOpen && (
-				<ul className="flex flex-col gap-2 bg-white absolute w-full rounded-lg top-14 p-4">
-					{options.map((option, index) => {
-						return (
-							<React.Fragment key={index}>
-								<li
-									className={`text-l-body text-medium-grey ${
-										disabled ? 'cursor-not-allowed' : 'cursor-pointer'
-									}`}
-									onClick={() => handleOptionClick(option)}
-								>
-									{option.label}
-								</li>
-							</React.Fragment>
-						);
-					})}
+				<ul className="flex flex-col gap-2 bg-white absolute w-auto min-w-[10rem] rounded-lg top-14 p-4">
+					<DropdownOptions
+						options={options}
+						disabled={disabled}
+						setIsOpen={setIsOpen}
+						setSelectedOption={setSelectedOption}
+					/>
+
+					<hr className="border-medium-grey/30" />
+
+					{actions.length > 0 && (
+						<DropdownActions setIsOpen={setIsOpen} actions={actions} />
+					)}
 				</ul>
 			)}
 		</div>
