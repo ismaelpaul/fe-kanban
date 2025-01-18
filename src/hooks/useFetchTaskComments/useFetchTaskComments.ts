ba57ca996e35e1afd597cf527bde2f848a5e342e
ismaelpaul <ismaafp@gmail.com>
2025-01-18 17:41:14 +0000

    Add taskId to queryKey and wrap comments in an object

--- src/hooks/useFetchTaskComments/useFetchTaskComments.ts
+++ src/hooks/useFetchTaskComments/useFetchTaskComments.ts
@@ -3,13 +3,13 @@ import { getTaskCommentsByTaskId } from '@/api/kanbanApi';
 import { useQuery } from '@tanstack/react-query';
 
 const useFetchTaskComments = (taskId: number) => {
-	const queryKey = ['comments'];
+	const queryKey = ['comments', taskId];
 
 	const { data, isLoading, isError } = useQuery(queryKey, () =>
 		getTaskCommentsByTaskId(taskId)
 	);
 
-	const comments = data || [];
+	const { comments } = data || [];
 
 	return { comments, isLoading, isError };
 };
