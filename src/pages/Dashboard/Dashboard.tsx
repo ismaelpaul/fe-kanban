fac2362020fc28ebf2984ba976e2727fa0f43f88
ismaelpaul <ismaafp@gmail.com>
2025-01-26 16:57:35 +0000

    Change Boards page for Dashboard page

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,17 +2,29 @@ import { useEffect, useState } from 'react';
 
 import { useFetchBoards } from '@/hooks';
 
-import { ColumnsList } from '@/components/Columns/ColumnsList';
-import { SidebarNav } from '@/components/Nav/Nav/SidebarNav';
-import { ToggleSidebar } from '@/components/Toggle/ToggleSidebar';
-
 import { useTeamsStore } from '@/store/teams';
 import { useBoardStore } from '@/store/boards';
+import { useModalStore } from '@/store/modals';
+
+import { ColumnsList } from '@/components/Columns/ColumnsList';
+import { ToggleNav } from '@/components/Toggle/ToggleNav/ToggleNav';
+import { SelectedBoard } from '@/components/Boards/SelectedBoard';
+import { SidebarNav } from '@/components/Nav/SidebarNav/SidebarNav';
+import { Button } from '@/components/Button';
+import { AddNewTaskModal } from '@/components/Tasks/AddNewTaskModal';
+import { AddNewBoardModal } from '@/components/Boards/AddNewBoardModal';
+import { EditBoardModal } from '@/components/Boards/EditBoardModal';
+import { MainSkeleton } from '@/components/SkeletonLoader/MainSkeleton';
+import { EditTeamModal } from '@/components/Teams/EditTeamModal';
+import { AddNewTeamModal } from '@/components/Teams/AddNewTeamModal';
 
 const Dashboard = () => {
-	const [isSidebarNavOpen, setIsSidebarNavOpen] = useState(false);
+	const [boardHasColumns, setBoardHasColumns] = useState(false);
+
+	const { modals, openModal } = useModalStore();
 
 	const selectedTeam = useTeamsStore((state) => state.selectedTeam);
+
 	const { selectedBoard, setSelectedBoard } = useBoardStore();
 
 	const teamId = selectedTeam.team_id;
@@ -23,27 +35,48 @@ const Dashboard = () => {
 		if (boards.length > 0) {
 			setSelectedBoard(boards[0]);
 		}
-	}, [boards]);
+	}, [selectedTeam, boards, setSelectedBoard]);
+
+	const btnAddNewTaskClass = `bg-purple py-2.5 px-5 rounded-full text-white tablet:text-m-heading transition ease-in-out duration-300 grow-1 ml-auto ${
+		boardHasColumns
+			? 'enabled:hover:bg-purple-hover'
+			: 'cursor-not-allowed opacity-75'
+	}`;
+
+	const openAddNewTaskModal = () => openModal('addNewTaskModal');
 
 	if (isLoading) {
-		return <div>Loading...</div>;
+		return (
+			<div>
+				<MainSkeleton />
+			</div>
+		);
 	}
 
 	return (
 		<>
-			{isSidebarNavOpen && (
-				<SidebarNav setIsSidebarNavOpen={setIsSidebarNavOpen} boards={boards} />
-			)}
-			<div className="flex flex-col ml-6">
-				<h1 className="text-black dark:text-white text-l-heading my-4">
-					{selectedBoard.name}
-				</h1>
-				<ColumnsList />
+			{modals.editBoardModal && <EditBoardModal />}
+			{modals.addNewBoardModal && <AddNewBoardModal />}
+			{modals.addNewTaskModal && <AddNewTaskModal />}
+			{modals.sidebarNav && <SidebarNav />}
+			{modals.editTeamModal && <EditTeamModal />}
+			{modals.addNewTeamModal && <AddNewTeamModal />}
+
+			<div className="flex flex-col px-6 overflow-x-scroll no-scrollbar w-full bg-light-bg dark:bg-dark-bg">
+				<div className="flex items-center gap-4 my-4">
+					<SelectedBoard selectedBoard={selectedBoard} />
+					<Button
+						onClick={openAddNewTaskModal}
+						buttonText={'+ Add New Task'}
+						buttonClass={btnAddNewTaskClass}
+					/>
+				</div>
+				<ColumnsList
+					boardHasColumns={boardHasColumns}
+					setBoardHasColumns={setBoardHasColumns}
+				/>
 			</div>
-			<ToggleSidebar
-				isSidebarNavOpen={isSidebarNavOpen}
-				setIsSidebarNavOpen={setIsSidebarNavOpen}
-			/>
+			<ToggleNav />
 		</>
 	);
 };
