7c1203c4dd7bf368d3487176fd5a55879cba17ac
ismaelpaul <ismaafp@gmail.com>
2025-03-13 19:01:46 +0000

    Add UserProfileSkeleton component for loading state

--- src/components/UserProfile/UserProfile/UserProfile.tsx
+++ src/components/UserProfile/UserProfile/UserProfile.tsx
@@ -8,6 +8,7 @@ import { useFetchUser, useToast } from '@/hooks';
 import { Button } from '@/components/Button';
 import { ChevronDown } from '@/components/SVGComponents/ChevronDown';
 import { ChevronUp } from '@/components/SVGComponents/ChevronUp';
+import { UserProfileSkeleton } from '@/components/SkeletonLoader/UserProfileSkeleton';
 
 const UserProfile = () => {
 	const [isLogoutOpen, setIsLogoutOpen] = useState(false);
@@ -24,10 +25,6 @@ const UserProfile = () => {
 	const logoutBtnClass =
 		'absolute px-4 py-[1.375rem] top-[4.2rem] bg-white dark:bg-dark-grey rounded-lg text-l-body text-medium-grey cursor-pointer w-32';
 
-	if (isLoading) {
-		return <span>Loading...</span>;
-	}
-
 	const handleLogout = async () => {
 		const result = await logoutUser();
 
@@ -40,6 +37,7 @@ const UserProfile = () => {
 
 	return (
 		<>
+			{isLoading && <UserProfileSkeleton />}
 			<div className="flex items-center gap-2">
 				<div className="ml-auto h-8 w-8 tablet:h-10 tablet:w-10 laptop:h-12 laptop:w-12 overflow-hidden rounded-full border-2 border-purple">
 					<img src={avatar} alt="Profile image" />
