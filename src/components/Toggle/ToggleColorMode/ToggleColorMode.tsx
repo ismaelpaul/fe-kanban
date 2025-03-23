9b7536f9eee5ea3fca019d8b2af2903cb11abe0b
ismaelpaul <ismaafp@gmail.com>
2025-03-23 10:50:17 +0000

    Add ml-6 and remove mx-4

--- src/components/Toggle/ToggleColorMode/ToggleColorMode.tsx
+++ src/components/Toggle/ToggleColorMode/ToggleColorMode.tsx
@@ -4,7 +4,7 @@ import { SwitchButton } from '@/components/SwitchButton';
 
 const ToggleColorMode = () => {
 	return (
-		<div className="bg-light-bg dark:bg-dark-bg flex items-center justify-center gap-6 h-12 rounded-md mt-4 mx-4">
+		<div className="bg-light-bg dark:bg-dark-bg flex items-center justify-center gap-6 h-12 ml-6 rounded-md mt-4">
 			<LightTheme />
 			<SwitchButton />
 			<DarkTheme />
