20c0409bd30be9709be53267b2da27ddb169658c
ismaelpaul <ismaafp@gmail.com>
2025-03-23 14:57:48 +0000

    Remove margin from h1 and add to its container

--- src/components/ModalHeader/ModalHeader/ModalHeader.tsx
+++ src/components/ModalHeader/ModalHeader/ModalHeader.tsx
@@ -7,8 +7,8 @@ type ModalHeaderProps = {
 
 const ModalHeader = ({ title, handleKebabMenu }: ModalHeaderProps) => {
 	return (
-		<div className="flex items-center justify-between gap-4">
-			<h1 className="text-l-heading dark:text-white mb-6">{title}</h1>
+		<div className="flex items-center justify-between gap-4 mb-6">
+			<h1 className="text-l-heading dark:text-white">{title}</h1>
 			<div onClick={handleKebabMenu} className="cursor-pointer">
 				<KebabMenuIcon />
 			</div>
