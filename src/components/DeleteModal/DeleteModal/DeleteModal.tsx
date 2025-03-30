1b76fc90813ceb7301d7e89864b02b4f62ef8b04
ismaelpaul <ismaafp@gmail.com>
2025-03-30 16:24:58 +0100

    Refactor component

--- src/components/DeleteModal/DeleteModal/DeleteModal.tsx
+++ src/components/DeleteModal/DeleteModal/DeleteModal.tsx
@@ -3,29 +3,20 @@ import { Overlay } from '@/components/Overlay';
 import { motion } from 'framer-motion';
 
 interface DeleteModalPros {
+	itemType: string;
 	itemName: string | null;
-	parentComponent: string;
 	setIsDeleteModalOpen: (arg: boolean) => void;
 	onClick: () => void;
 }
 
 const DeleteModal = ({
+	itemType,
 	itemName,
-	parentComponent,
 	setIsDeleteModalOpen,
 	onClick,
 }: DeleteModalPros) => {
-	const deleteBtnTex = 'Delete';
-	const deleteBtnClass =
-		'bg-red text-white text-13px font-bold py-2 w-full rounded-full tablet:mb-0 tablet:py-3 hover:bg-red-hover';
-
-	const cancelBtnText = 'Cancel';
-	const cancelBtnClass =
-		'bg-purple bg-opacity-10 dark:bg-white text-purple text-13px font-bold py-2 w-full rounded-full tablet:py-3 hover:bg-purple/25';
-
-	const item = parentComponent === 'Nav' ? 'board' : 'task';
 	const complementaryText =
-		item === 'board'
+		itemType === 'board'
 			? ' board? This action will remove all columns and tasks and '
 			: 'task and its subtasks? This action ';
 
@@ -42,7 +33,7 @@ const DeleteModal = ({
 				aria-labelledby="modal-heading"
 			>
 				<h1 id="modal-heading" className="text-l-heading text-red mb-6">
-					Delete this {item}?
+					Delete this {itemType}?
 				</h1>
 				<p className="text-l-body text-medium-grey mb-6">
 					Are you sure you want to delete the '{itemName}' {complementaryText}{' '}
@@ -50,13 +41,17 @@ const DeleteModal = ({
 				</p>
 				<div className="flex flex-col tablet:flex-row gap-4">
 					<Button
-						buttonText={deleteBtnTex}
-						buttonClass={deleteBtnClass}
+						buttonText={'Delete'}
+						buttonClass={
+							'bg-red text-white text-13px font-bold py-2 w-full rounded-full tablet:mb-0 tablet:py-3 hover:bg-red-hover'
+						}
 						onClick={onClick}
 					/>
 					<Button
-						buttonText={cancelBtnText}
-						buttonClass={cancelBtnClass}
+						buttonText={'Cancel'}
+						buttonClass={
+							'bg-purple bg-opacity-10 dark:bg-white text-purple text-13px font-bold py-2 w-full rounded-full tablet:py-3 hover:bg-purple/25'
+						}
 						onClick={() => setIsDeleteModalOpen(false)}
 					/>
 				</div>
