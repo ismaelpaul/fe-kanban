0d1eb01a2345f9782cf28778cce432bebde00099
ismaelpaul <ismaafp@gmail.com>
2025-03-31 20:28:10 +0100

    Move WebsocketMessage interface to WebsocketContext file

--- src/interfaces/IWebSocket.ts
+++ src/interfaces/IWebSocket.ts
@@ -1,8 +1,3 @@
-export interface WebSocketMessage<T = unknown> {
-	type: string;
-	payload: T;
-}
-
 export interface WebSocketContextType {
 	ws: WebSocket | null;
 	sendMessage: <T extends object>(type: string, payload: T) => void;
